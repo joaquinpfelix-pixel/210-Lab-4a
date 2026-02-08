9d8676e04a5967406c2a8111070d1faf046b4625
JoaquinF <joaquinpfelix@gmail.com>
2026-02-08 15:29:17 -0800

    Check 6

--- 210_LAB_4a.cpp
+++ 210_LAB_4a.cpp
@@ -10,17 +10,22 @@ struct Color {
 };
 
 int main() {
-
-    Color testColor = {128, 39, 95};
+    srand(time(0)); 
 
     vector<Color> colorVector;
-    colorVector.push_back(testColor);
     
-
-
-
-
     
+    int randomNumber = rand() % 26 + 25;
+
+    FOR (int i = 0; i < randomNumber; i++){
+        Color testColor;
+        testColor.red = rand() % 26 +25;
+        testColor.green = rand() % 26 +25;
+        testColor.blue = rand() % 26 +25;
+        colorVector.push_back(testColor);
+    }
+
+
     cout << "Red: " << colorVector[0].red << endl; 
     cout << "Green: " << colorVector[0].green << endl; 
     cout << "Blue: " << colorVector[0].blue << endl;
