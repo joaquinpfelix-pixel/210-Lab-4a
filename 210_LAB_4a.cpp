f1d4618227e82a9ce023215547166e08def26d6c
JoaquinF <joaquinpfelix@gmail.com>
2026-02-08 15:06:16 -0800

    Check 4 I had the same problem where that one file the code didn't run right, so I made a new one and moved it over

--- 210_LAB_4a.cpp
+++ 210_LAB_4a.cpp
@@ -12,8 +12,15 @@ struct Color {
 int main() {
 
     Color testColor = {128, 39, 95};
+
+    vector<Color> colorVector;
+    colorVector.push_back(testColor);
+    
+
     cout << "Red: " << testColor.red << " " << endl; 
     cout << "Green: " << testColor.green << " " <<endl; 
     cout << "Blue: " << testColor.blue << " " << endl;
+
+
 }
 
